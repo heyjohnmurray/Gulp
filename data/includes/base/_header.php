aa02a06618b308d709302c7364fcd4d5347d2b3e
John <jmurray@redventures.com>
2014-08-20 12:14:40 -0400

    making good progress. just doing a quick commit and deploy to staging

--- data/includes/base/_header.php
+++ data/includes/base/_header.php
@@ -6,7 +6,7 @@
 
 <!-- CSS -->
 <link href="/assets/css/compiled.css" rel="stylesheet">
-<link href="/assets/css/jquery.mobile-1.4.3.css" rel="stylesheet">
+<!-- <link href="/assets/css/jquery.mobile-1.4.3.css" rel="stylesheet"> -->
 <link href="/assets/css/jquery.mobile.structure-1.4.3.css" rel="stylesheet">
 <!-- <link href="/assets/css/jquery.mobile.theme-1.4.3.css" rel="stylesheet"> -->
 
@@ -15,14 +15,22 @@
 <script src="/assets/js/jquery.mobile-1.4.3.js"></script>
 
 <script type="text/javascript">
+	$( window ).on( "orientationchange", function( event ) {
+		//$( "#orientation" ).text( "This device is in " + event.orientation + " mode!" );
+		if (event.orientation === 'portrait') {
+			alert('please view this app in landscape mode');
+		};
+	});
+
 	$('#signup').live('pagecreate',function(event) { 
 		$('.signup-form').submit( function (e) {
-			$(':mobile-pagecontainer').pagecontainer('change', '#questions', {
-		        transition: 'slide',
-		        changeHash: false
+			$(':mobile-pagecontainer').pagecontainer('change', '#slide1', {
+		        transition: 'slide'
 		    });
 		    e.preventDefault();
 		});
 	});
+
+
 </script>
 </head>
