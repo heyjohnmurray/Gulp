2fdfb6f7489865f77d4741404dd494f49cbe6c30
Matt Burke <mburke@rv-clt-mburke.redventures.net>
2014-08-28 17:41:37 -0400

    reset scripts

---
+++ data/reseed.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE BlendConf.Users;
+
+INSERT INTO BlendConf.Users(FirstName, LastName, Email, Validated) VALUES
+('Matt', 'Burke', 'mburke@redventures.com', 0),
+('John', 'Murray', 'jmurray@redventures.com', 0),
+('Brian', 'Bachtel', 'bbachtel@redventures.com', 0);
