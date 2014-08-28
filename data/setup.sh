97967a1a04d75635f26e264bc9ef0498efd5ce52
Matt Burke <mburke@rv-clt-mburke.redventures.net>
2014-08-28 17:41:58 -0400

    setup script

---
+++ data/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+brew install node
+brew install mysql
+mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
+mysql.server start
+cat default.sql | mysql -u root -h localhost
+reset.sh
