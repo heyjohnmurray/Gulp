a9cb3bb8f235a10dedf9877520f93a5ebb9ea45d
Brian Bachtel <bbachtel@rv-clt-bbachtel.redventures.net>
2014-09-02 18:16:06 -0400

    remove angular from app

--- data/public/js/controllers/main.js
+++ data/public/js/controllers/main.js
@@ -3,6 +3,10 @@ angular.module('mainController', [])
 	.controller('mainController', function($scope, $http, $location) {
         $scope.formData = {};
 
+        $scope.$on("$routeChangeSuccess", function(){
+                $( ".wrapper" ).pagecontainer( "load");
+                console.log('woohoo');
+        });
 
         // when submitting the login form, send the form to the node API
         $scope.login = function() {
