97bf3c7b690e4b5da3d2733f08b4c700364a3d15
Brian Bachtel <bbachtel@rv-clt-bbachtel.redventures.net>
2014-09-04 11:27:22 -0400

    the datas

--- data/default.sql
+++ data/default.sql
@@ -10,7 +10,40 @@ CREATE TABLE `BlendConf`.`Users` (
   `LastName` varchar(255) DEFAULT NULL,
   `Email` varchar(255) DEFAULT NULL,
   `Validated` tinyint(1) DEFAULT '0',
+  `PollResult` tinyint(2) DEFAULT NULL,
+  `Started` datetime DEFAULT NULL,
+  `Completed` datetime DEFAULT NULL,
   PRIMARY KEY (`UserID`),
   KEY `FirstNameLastName` (`FirstName`,`LastName`),
   KEY `Validated` (`Validated`)
 );
+
+CREATE TABLE `UserVotes` (
+  `UserVoteID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `UserID` int(11) DEFAULT NULL,
+  `PollID` int(11) DEFAULT NULL,
+  `Vote` tinyint(11) DEFAULT NULL,
+  PRIMARY KEY (`UserVoteID`),
+  UNIQUE KEY `UserID_PollID` (`UserID`,`PollID`)
+);
+
+CREATE TABLE `Polls` (
+  `PollID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Answer` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`PollID`)
+);
+
+INSERT INTO `Polls` (`PollID`, `Answer`)
+VALUES
+	(1,1),
+	(2,2),
+	(3,2),
+	(4,2),
+	(5,2),
+	(6,1),
+	(7,1),
+	(8,2),
+	(9,2),
+	(10,2),
+	(11,1),
+	(12,1);
