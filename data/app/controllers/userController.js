b88933d81ba3663821e4ab4e4e2b5d9a3f7fc971
Matt Burke <mburke@redventures.com>
2014-09-08 14:31:09 -0400

    format

--- data/app/controllers/userController.js
+++ data/app/controllers/userController.js
@@ -2,69 +2,69 @@ var deps = require('../../config/db');
 
 function isUserAuthenticated(firstName, lastName, callback) {
 
-	deps.db.query('SELECT UserID, Validated FROM BlendConf.Users WHERE FirstName = ? AND LastName = ?', [firstName,lastName],function(err, rows) {
-			if (err) return callback(err);
-			if (!rows[0]) return callback({'message':'Unknown User'});
-			if (rows[0].Validated) return callback({message: 'Already Voted'});
+  deps.db.query('SELECT UserID, Validated FROM BlendConf.Users WHERE FirstName = ? AND LastName = ?', [firstName,lastName],function(err, rows) {
+      if (err) return callback(err);
+      if (!rows[0]) return callback({'message':'Unknown User'});
+      if (rows[0].Validated) return callback({message: 'Already Voted'});
 
-			try {
-		      	callback(null, {'userID':rows[0].UserID});
-		    } catch (e) {
-		      callback(e);
-		    }
+      try {
+            callback(null, {'userID':rows[0].UserID});
+        } catch (e) {
+          callback(e);
+        }
 
-	});
+  });
 }
 
 function setUserAuthenticated(email, userID, callback) {
 
-	deps.db.query('UPDATE BlendConf.Users SET Email = ?, Validated = 1, Started = NOW() WHERE UserID = ? ', [email,userID],function(err, row) {
-			if (err) return callback(err);
-			if (row.changedRows === 0) return callback({'message':'Could not update User'});
+  deps.db.query('UPDATE BlendConf.Users SET Email = ?, Validated = 1, Started = NOW() WHERE UserID = ? ', [email,userID],function(err, row) {
+      if (err) return callback(err);
+      if (row.changedRows === 0) return callback({'message':'Could not update User'});
 
-		    callback(null, row);
-	});
+        callback(null, row);
+  });
 }
 
 function getUserResult(userID, callback) {
 
-	deps.db.query('SELECT PollResult FROM BlendConf.Users WHERE UserID = ? AND PollResult IS NOT NULL', [userID],function(err, row) {
-			if (err) return callback(err);
-			if (!row[0]) return callback({'message':'No Results for user'});
+  deps.db.query('SELECT PollResult FROM BlendConf.Users WHERE UserID = ? AND PollResult IS NOT NULL', [userID],function(err, row) {
+      if (err) return callback(err);
+      if (!row[0]) return callback({'message':'No Results for user'});
 
-			try {
-		      	callback(null, {'pollResult':row[0].PollResult});
-		    } catch (e) {
-		      callback(e);
-		    }
+      try {
+            callback(null, {'pollResult':row[0].PollResult});
+        } catch (e) {
+          callback(e);
+        }
 
-	});
+  });
 }
 
 function getTopUsers(callback) {
-	var userQuery = "SELECT CONCAT(FirstName,' ',LastName) AS Name, PollResult, TIMEDIFF(Completed,Started) AS TimeCompleted, TIME_FORMAT(TIMEDIFF(Completed,Started), '%i:%s') AS FormatedTimeCompleted FROM BlendConf.Users WHERE PollResult IS NOT NULL AND Completed IS NOT NULL HAVING TimeCompleted > 0 ORDER BY PollResult DESC, TimeCompleted ASC LIMIT 10";
+  var userQuery = "SELECT CONCAT(FirstName,' ',LastName) AS Name, PollResult, TIMEDIFF(Completed,Started) AS TimeCompleted, TIME_FORMAT(TIMEDIFF(Completed,Started), '%i:%s') AS FormatedTimeCompleted FROM BlendConf.Users WHERE PollResult IS NOT NULL AND Completed IS NOT NULL HAVING TimeCompleted > 0 ORDER BY PollResult DESC, TimeCompleted ASC LIMIT 10";
 
-	deps.db.query(userQuery, function(err, rows) {
-			if (err) return callback(err);
-			if (!rows[0]) return callback({'message':'No Results'});
+  deps.db.query(userQuery, function(err, rows) {
+      if (err) return callback(err);
+      if (!rows[0]) return callback({'message':'No Results'});
 
-		    callback(null, {'topResults':rows});
+        callback(null, {'topResults':rows});
 
-	});
+  });
 }
 
 function addUser(firstName, lastName, email, callback) {
 
-	deps.db.query('INSERT INTO BlendConf.Users (FirstName, LastName, Email, Validated, Started) VALUES (?,?,?,1,NOW())', [firstName,lastName,email],function(err, rows) {
-			if (err) return callback(err);
+  deps.db.query('INSERT INTO BlendConf.Users (FirstName, LastName, Email, Validated, Started) VALUES (?,?,?,1,NOW())', [firstName,lastName,email],function(err, rows) {
+      if (err) return callback(err);
 
-			try {
-		      	callback(null, {'userID':rows.insertId});
-		    } catch (e) {
-		      callback(e);
-		    }
+      try {
+            callback(null, {'userID':rows.insertId});
+        } catch (e) {
+          callback(e);
+        }
 
-	});
+  });
 }
 
 function addNewUser(firstName, lastName, callback) {
@@ -86,6 +86,7 @@ function addNewUser(firstName, lastName, callback) {
 }
 
 function resetUser(user, callback) {
+
   deps.db.query("UPDATE BlendConf.Users SET Email='', Validated=0, PollResult=NULL, Started=NULL, Completed=NULL WHERE FirstName = ? AND LastName = ?", [user.FirstName, user.LastName], function(err, rows) {
     if (err) return callback(err);
 
