ab87059c229c447d8e58d7e4688c77cdcb114aa1
James Huston <jhuston@redventures.com>
2014-09-25 11:45:27 -0400

    move gulp to the top level and got error handling working for SASS builds

--- gulpfile.js
+++ gulpfile.js
@@ -26,27 +26,41 @@ var currDate = new Date();
 //default 'gulp' task
 gulp.task('default', ['sass']);
 
+/**
+ * Print out some friendly info on an error and end
+ * cleanly so that watch doesn't break.
+ *
+ * @param {Error} err
+ */
+function handleError(err) {
+  console.log('Plugin error: ' + err.plugin);
+  console.log('Message: ' + err.message);
+
+  this.emit('end');
+}
+
 //sass task
 gulp.task('sass', function(){
-
-	return gulp.src('assets/sass/**/*.scss')
-		.pipe(sass())
+	var stream = gulp.src('data/assets/sass/**/*.scss')
+		.pipe(sass().on('error', handleError))
 		.pipe(header('/* compiled at ' + currDate.getHours() + ':' + currDate.getMinutes() + ':' + currDate.getSeconds() + ' on ' + (currDate.getMonth()+1) + '-' + currDate.getDate() + '-' + currDate.getFullYear() + ' */' + '\n'))//compiled time stamp
-		.pipe(gulp.dest('assets/css/'))
+		.pipe(gulp.dest('data/assets/css/'))
 		.pipe(livereload())
 		.pipe(notify({
 			message: 'SASS has been compiled'
 		}));
+
+  return stream;
 });
 
 //js task :: this task could probably have gulp-changed added to it so it only compresses changed js files
 gulp.task('js', function(){
-	return gulp.src('assets/js/**/*.js')//right now this compiles everything in the js folder, but you could pass an array of files like   gulp.src(['public/js/*.js', 'bower_components/**/*.js'])
-		.pipe(uglify('project.min.js', {
+	return gulp.src('data/assets/js/**/*.js')//right now this compiles everything in the js folder, but you could pass an array of files like   gulp.src(['public/js/*.js', 'bower_components/**/*.js'])
+		.pipe(uglify('data/project.min.js', {
 			outSourceMap: true
 		}))
 		.pipe(header('/* compiled at ' + currDate.getHours() + ':' + currDate.getMinutes() + ':' + currDate.getSeconds() + ' on ' + (currDate.getMonth()+1) + '-' + currDate.getDate() + '-' + currDate.getFullYear() + ' */' + '\n'))//compiled time stamp
-		.pipe(gulp.dest('assets/js/'))
+		.pipe(gulp.dest('data/assets/js/'))
 		.pipe(notify({
 			message: 'JS has been compiled' //no 'message' parameter defaults to list of files affected
 		}));
@@ -54,27 +68,27 @@ gulp.task('js', function(){
 
 //images
 gulp.task('images', function () {
-  return gulp.src('assets/images/**/*')
-    .pipe(changed('assets/images-min/'))
+  return gulp.src('data/assets/images/**/*')
+    .pipe(changed('data/assets/images-min/'))
     .pipe(imagemin({
     	optimizationLevel: 3,
     	progressive: true,
     	interlaced: true
     	}))
-    .pipe(gulp.dest('assets/images-min/'));
+    .pipe(gulp.dest('data/assets/images-min/'));
 });
 
 //sprites
 
 // THIS SYNTAX WORKS BUT IT'S NOT WHAT I USE FOR EVERYTHING ELSE
 gulp.task('sprites', function () {
-  var spriteData = gulp.src('assets/images/sprites/**/*')
+  var spriteData = gulp.src('data/assets/images/sprites/**/*')
   .pipe(spritesmith({
-    imgName: 'assets/images/sprite.png',
+    imgName: 'data/assets/images/sprite.png',
     cssName: '_sprites.scss'
   }));
-  spriteData.img.pipe(gulp.dest('assets/images/'));
-  spriteData.css.pipe(gulp.dest('assets/sass/project/partials/'));
+  spriteData.img.pipe(gulp.dest('data/assets/images/'));
+  spriteData.css.pipe(gulp.dest('data/assets/sass/project/partials/'));
 });
 
 // THIS IS THE SYNTAX I WANT BUT IT'S NOT WORKING QUITE RIGHT.
@@ -93,14 +107,14 @@ gulp.task('watch', function(){
   livereload.listen();
 
   // watch our files that we don't build but run through the webserver.
-  gulp.watch('**/*.html').on('change', livereload.changed);
-  gulp.watch('**/*.php').on('change', livereload.changed);
+  gulp.watch('data/**/*.html').on('change', livereload.changed);
+  gulp.watch('data/**/*.php').on('change', livereload.changed);
 
   //sass watch
-	gulp.watch('assets/sass/**/*.scss', ['sass']); //watch any sass file change
+	gulp.watch('data/assets/sass/**/*.scss', ['sass']); //watch any sass file change
 	//js watch
   //watch only root-level .js file change :: don't care about lib or plugin folders
-	gulp.watch('assets/js/*.js', ['js']);
+	gulp.watch('data/assets/js/*.js', ['js']);
 
 });
 
